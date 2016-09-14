b1d068023f3a1b35da7237b8e649ef9903fa096e
Kaushal Kumar <kaushal499@gmail.com>
2016-09-14 12:56:03 +0530

    hero object add .. template update

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,27 @@
 import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: '<h1>My SECOND Angular 2 App</h1>'
+  template:`
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input value="{{hero.name}}" placeholder="name">
+  </div>
+  `
 })
-export class AppComponent { }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+  hero: Hero = {
+  id: 1,
+  name: 'Windstorm'
+  };
+}
