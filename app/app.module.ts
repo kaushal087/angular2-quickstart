de3c0464323f45ce7df92a9a37a3386f663c7891
Kaushal Kumar <kaushal499@gmail.com>
2016-09-15 18:10:10 +0530

    removed compiled files, Hero5 update

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,30 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroService }          from './hero.service';
+import { routing } from './app.routing';
+
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    routing
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
   ],
   bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -6,6 +6,12 @@ export class HeroService {
   getHeroes(): Promise<Hero[]> {
     return Promise.resolve(HEROES);
   }
+  
+
+getHero(id: number): Promise<Hero> {
+  return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+}
+
 }
 
 
